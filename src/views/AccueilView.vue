3a9e9325c27998b803cd4bfe9c71ddd91e44b31a
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 09:14:35 +0200

    debut page d'accueil

---
+++ src/views/AccueilView.vue
@@ -0,0 +1,8 @@
+<template>
+    <main class="text-white py-10">
+        <div class="sm:mx-6 md:mx-8 lg:mx-10">
+            <h1 class="text-xl  md:text-3xl font-timmana text-center py-7  ">N'attendez plus et venez passer une
+                agreable moment avec vos rappeurs preferes</h1>
+        </div>
+    </main>
+</template>
