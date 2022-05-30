165f0b84b6f57dba91fd2cd48b2ec3c256ca222c
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 10:46:08 +0200

    debut contact

---
+++ src/views/ContactView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="mx-4 md:mx-8 lg:mx-10 py-10  text-white">
+        <h1 class="text-xl md:text-3xl   py-10 font-timmana text-center underline-offset-2 underline ">ENVOYER UN MESSAGE</h1>
+    </div>
+</template>
