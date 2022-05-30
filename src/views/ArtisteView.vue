3f41d552c674db5410923ca135ab7574a1c150ee
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 10:58:37 +0200

    debut page artiste

---
+++ src/views/ArtisteView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="mx-4 md:mx-8 lg:mx-10 py-10 text-white text-center">
+        <h1 class="text-xl md:text-3xl   py-10 font-timmana underline-offset-2 underline">Leal Briard</h1>
+        <img class="m-auto rounded-full md:w-1/3 w-4/5 my-10" src="../../public/img/Image8.webp" alt="">
+    </div>
+</template>
