ab738c23fcb9ca421028373c798fd74bb16a1bda
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 10:41:35 +0200

    debut festival

---
+++ src/views/FestivalView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="mx-4 md:mx-8 lg:mx-10 py-10 text-white text-center">
+        <h1 class="text-xl md:text-3xl   py-10 font-timmana underline-offset-2 underline ">L'HISTOIRE DE NOTRE FESTIVAL</h1>
+        <h2 class="text-lg md:text-2xl   py-10 font-timmana">Festival</h2>
+        <p class=" font-poppins text-base md:text-lg   ">Ici vous pourrez trouver l'histoire de notre festival qui va devenir une grande événement dans le futur</p>
+    </div>
+</template>
