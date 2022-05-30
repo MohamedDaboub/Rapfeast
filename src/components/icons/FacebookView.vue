0d1101bb1d9e3bdebe305c8292fc8a5519f1221e
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 08:57:11 +0200

    icon facebook + instagram

---
+++ src/components/icons/FacebookView.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="w-6 h-6 lg:w-12 lg:h-12 relative"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M13.397 20.997V12.801H16.162L16.573 9.59202H13.397V7.54802C13.397 6.62202 13.655 5.98802 14.984 5.98802H16.668V3.12702C15.8487 3.03921 15.0251 2.99681 14.201 3.00002C11.757 3.00002 10.079 4.49202 10.079 7.23102V9.58602H7.33203V12.795H10.085V20.997H13.397Z"
+      fill="white"
+    ></path>
+  </svg>
+</div>
+</template>
