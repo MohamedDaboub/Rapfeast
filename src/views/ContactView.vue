82206ebe285ca1570e0a89acef2f32c09fcac4a5
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 10:51:20 +0200

    fin page contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -41,6 +41,13 @@
                     </div>
                 </form>
             </div>
+            <div class="text-center">
+                <h3 class="text-xl md:text-3xl  font-timmana my-10">Ou nous trouver ?</h3>
+                <address class="text-base md:text-lg font-poppins">3 Boulevard O, 25000 Besançon</address>
+                <div class="w-full">
+                    <iframe class=" w-full flex m-0 py-10 " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2709.7036977257835!2d5.977912615560326!3d47.22237967916118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478d638eef0d7625%3A0xd85a2e4d7709f4d4!2sMicropolis%20Besan%C3%A7on!5e0!3m2!1sfr!2sfr!4v1653595741553!5m2!1sfr!2sfr" height="500" style="border:0;"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
         </section>
     </div>
 </template>
