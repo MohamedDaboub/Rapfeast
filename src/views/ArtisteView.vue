fd8ed93b4b179a6736ee4776cfe4f0a691ffc8c9
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 11:08:01 +0200

    icon person +

--- src/views/ArtisteView.vue
+++ src/views/ArtisteView.vue
@@ -4,3 +4,15 @@
         <img class="m-auto rounded-full md:w-1/3 w-4/5 my-10" src="../../public/img/Image8.webp" alt="">
     </div>
 </template>
+<script>
+import person from "../components/icons/PersonView.vue"
+import fight from "../components/icons/FightView.vue"
+import Facebook from "../components/icons/FacebookView.vue"
+import Instagram from "../components/icons/InstagramView.vue"
+import Twitter from "../components/icons/TwitterView.vue"
+import Youtube from "../components/icons/YoutubeView.vue"
+export default {
+    name: "App",
+    components: { fight,person,Facebook,Instagram,Twitter,Youtube,  },
+}
+</script>
