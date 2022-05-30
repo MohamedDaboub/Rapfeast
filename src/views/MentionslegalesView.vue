437aa9c83f463fe585091657094ad47df67fb15d
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 11:15:28 +0200

    debut Mentions legales

--- src/views/MentionslegalesView.vue
+++ src/views/MentionslegalesView.vue
@@ -1,4 +1,6 @@
 <template>
-
+    <body class="mx-4 md:mx-8 lg:mx-10 py-10 text-center text-white">
+        <h1 class="text-xl md:text-3xl   py-10 font-timmana underline-offset-2 underline ">Mentions legales</h1>
+    </body>
   
 </template>
