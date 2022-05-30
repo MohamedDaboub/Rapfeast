99550eb3b43b2cf514d3971c5a7bad0b46614c63
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 09:52:28 +0200

    card en vue

---
+++ src/components/CardView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div >
+        <figure class=" flex flex-col justify-center items-center mx-auto my-10 rounded-lg md:w-1/3     ">
+            <img class=" rounded-t-lg  " :src="image" alt="">
+            <figcaption class=" flex px-5 py-4 bg-Default/me/Gris  ">
+                <div class="  flex flex-col w-full ">
+                    <h3 class="text-lg text-white text-center py-4 font-poppins ">{{ nom }}</h3>
+                    <RouterLink class="px-8 py-4 bg-Default/me/Noir  flex justify-end items-end m-auto text-base font-poppins" to="/">Découvrez</RouterLink>
+                </div>
+            </figcaption>
+        </figure>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        nom: String,
+        image: String, 
+        router:String,
+    },
+       
+    
+    components:{}
+}
+</script>
--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,13 +1,13 @@
 <template>
     <main class="text-white py-10">
-        <div class="sm:mx-6 md:mx-8 lg:mx-10">
+        <div class="mx-4 md:mx-8 lg:mx-10">
             <h1 class="text-xl  md:text-3xl font-timmana text-center py-7  ">N'attendez plus et venez passer une
                 agreable moment avec vos rappeurs preferes</h1>
             <div>
                 <img class="flex m-auto text-center w-11/12" src="../../public/img/Image1.webp" alt="image d'un festival rap feast">
             </div>
             <flech class="flex text-center m-auto my-12 fill-white" />
-            <h2 class="sm:text-lg  md:text-2xl font-timmana text-center py-7  ">A propos de notre Festival</h2>
+            <h2 class="text-lg  md:text-2xl font-timmana text-center py-7  ">A propos de notre Festival</h2>
         </div>
         <div class=" relative">
             <img class="md:w-full md:h-full h-[410px] w-full  " src="../../public/img/Image2.webp" alt="">
