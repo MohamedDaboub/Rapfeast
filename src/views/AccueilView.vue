4e00eee03eec79021abbe8b70030d43be5999341
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 09:15:30 +0200

    image importer

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -3,6 +3,9 @@
         <div class="sm:mx-6 md:mx-8 lg:mx-10">
             <h1 class="text-xl  md:text-3xl font-timmana text-center py-7  ">N'attendez plus et venez passer une
                 agreable moment avec vos rappeurs preferes</h1>
+            <div>
+
+            </div>
         </div>
     </main>
 </template>
