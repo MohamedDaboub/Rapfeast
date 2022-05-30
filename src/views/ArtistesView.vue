55bdbdbc3433eb11ec45044e7c8c04722d21666a
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 10:35:00 +0200

    debut artistes en vue

---
+++ src/views/ArtistesView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="mx-4 md:mx-8 lg:mx-10 py-10 text-center text-white">
+        <h1 class="text-xl md:text-3xl   py-10 font-timmana underline-offset-2 underline ">NOS DIFFERENTS ARTISTES</h1>
+        <h2 class="text-lg md:text-2xl   py-10 font-timmana">Les artistes des concerts </h2>
+        <p class=" font-poppins text-base md:text-lg   ">Vous pouvez ici trouver tous nos différents artistes et réserver votre ticket depuis cette page</p>
+    </div>
+</template>
