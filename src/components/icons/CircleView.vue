c93c0c46e39b3721aed11e6e1c2163e6aaabb02f
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 09:40:13 +0200

    partie imprtant âge d'accueil plus icone circle trending up

---
+++ src/components/icons/CircleView.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg
+  width="104"
+  height="104"
+  viewBox="0 0 104 104"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-4/5 md:w-full"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <circle cx="52" cy="52" r="52" fill="#B91C1C"></circle>
+</svg>
+</template>
--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -9,13 +9,34 @@
             <flech class="flex text-center m-auto my-12 fill-white" />
             <h2 class="sm:text-lg  md:text-2xl font-timmana text-center py-7  ">A propos de notre Festival</h2>
         </div>
+        <div class=" relative">
+            <img class="md:w-full md:h-full h-[410px] w-full  " src="../../public/img/Image2.webp" alt="">
+            <h1 class=" absolute left-[40%] top-[10%] md:text-4xl text-xl font-timmana">RAP FEAST</h1>
+            <p class="absolute text-base mr-6 left-[15%] top-1/3 md:mr-44 font-poppins md:text-xl leading-tight">Et un
+                festival qui se passe chaque année à Besançon et dans lequel nous allons inviter les meilleurs rappeurs
+                de France chaque année on a pu de 10000 visiteurs !</p>
+            <h3 class=" md:text-4xl text-xl font-timmana text-red-700  absolute top-[60%] left-1/4">Juillet</h3>
+            <h3 class=" md:text-4xl text-xl font-timmana  text-red-700 absolute top-[60%] right-1/4 ">2022</h3>
+            <h4 class="text-lg md:text-2xl  font-timmana absolute bottom-[20%] left-[25%] md:left-[30%]">3 JOUR</h4>
+            <h4 class="text-lg md:text-2xl  font-timmana absolute bottom-[20%] right-[25%] md:right-[30%]">9 GROUPE</h4>
+            <div class="absolute bottom-[0%] left-[38%] md:left-[45%] md:bottom-[5%] ">
+                <div class="relative">
+                    <cirecle class="relative"></cirecle>
+                    <TrendingUpIcon class="absolute top-1/4 md:right-[15%] w-1/4 right-[20%] "></TrendingUpIcon>
+                    <p class="absolute top-1/4 left-[5%] ">Acheter <br> des billets</p>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 <script>
 import flech from "../components/icons/FlechbasView.vue"
+import { TrendingUpIcon, } from "@heroicons/vue/solid";
+import cirecle from "../components/icons/CircleView.vue"
+
 
 export default {
     name: "App",
-    components: { flech, },
+    components: { flech,cirecle,TrendingUpIcon },
 };
 </script>
