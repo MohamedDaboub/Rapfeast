7323287a5d589e8cdbf896cf31d5961788c4e50a
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 10:20:43 +0200

    debut page programmation

--- src/views/ProgrammationView.vue
+++ src/views/ProgrammationView.vue
@@ -1,6 +1,9 @@
 <template>
-
-    <h1 class="text text-3xl text-white text-center py-10">NOS PROGRAMMATION</h1>
+    <div class="mx-4 md:mx-8 lg:mx-10">
+        <h1 class="text-xl md:text-3xl text-white text-center py-10 font-timmana underline-offset-2 underline ">NOS PROGRAMMATION</h1>   
+        <h2 class="text-lg md:text-2xl text-white text-center py-10 font-timmana">Nos differents concerts</h2>
+        <p class=" font-poppins text-base md:text-lg text-center text-white ">Vous pouvez retrouver toutes les dates des concerts et leurs horaires au cours de ces 3 jours</p>
+    </div>
     
 
 </template>
