98e64d3986a8c3fe74569d672469fab29108f9f1
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 09:24:46 +0200

    flech classe + h2

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -6,6 +6,8 @@
             <div>
                 <img class="flex m-auto text-center w-11/12" src="../../public/img/Image1.webp" alt="image d'un festival rap feast">
             </div>
+            <flech class="flex text-center m-auto my-12 fill-white" />
+            <h2 class="sm:text-lg  md:text-2xl font-timmana text-center py-7  ">A propos de notre Festival</h2>
         </div>
     </main>
 </template>
