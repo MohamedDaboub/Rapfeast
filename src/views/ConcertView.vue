6496729793f6d46fc7b0d818b7fb9d9c680a4fd1
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 10:54:07 +0200

    icon time

--- src/views/ConcertView.vue
+++ src/views/ConcertView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mx-4 md:mx-8 lg:mx-12 xl:mx-16 py-10 text-white text-center">
         <h1 class="text-xl md:text-3xl   py-10 font-timmana underline-offset-2 underline">CONCERT DE RAP</h1>
-        <img src="../../public/img-squirrel/Image7.webp" class="m-auto rounded-sm my-10" alt="">
+        <img src="../../public/img/Image7.webp" class="m-auto rounded-sm my-10" alt="">
         <p class="text-lg md:text-xl my-10 text-left xl:mx-[25%] font-poppins">Le concert de <span class="text-red-700"> David Aupry</span> a l’honneur de vous accueillir,
         voici quelques détails sur le concert.</p>
     </div>
