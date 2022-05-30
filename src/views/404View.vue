aba2a44fac0b502b9261abb667c3f73e5f0f4942
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-30 11:16:28 +0200

    debut page 404

---
+++ src/views/404View.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="mx-4 md:mx-8 lg:mx-10 py-10 text-white text-center">
+        <h1 class="text-xl md:text-3xl py-10 font-timmana underline-offset-2 underline">ERREUR 404</h1>
+</div>
+</template>
