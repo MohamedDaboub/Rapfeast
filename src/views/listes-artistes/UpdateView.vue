115f8ce99786360a5e221f23e8b61e0850f35dc4
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-06-02 22:39:36 +0200

    css pour page delete + update

--- src/views/listes-artistes/UpdateView.vue
+++ src/views/listes-artistes/UpdateView.vue
@@ -1,3 +1,3 @@
 <template>
-    
+          <h2 class="text-xl md:text-3xl text-center py-10 font-timmana underline-offset-2 underline effet-shawdow light:effet-shawdowinligth">Mise à jour d'un artiste</h2>
 </template>
