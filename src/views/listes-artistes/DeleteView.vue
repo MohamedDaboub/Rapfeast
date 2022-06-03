28be88f501ff65bf15869fe8416e9008b7377553
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-06-03 08:31:48 +0200

    button dans page delete vue

--- src/views/listes-artistes/DeleteView.vue
+++ src/views/listes-artistes/DeleteView.vue
@@ -1,3 +1,12 @@
 <template>
     <h2 class="text-xl md:text-3xl text-center py-10 font-timmana underline-offset-2 underline effet-shawdow light:effet-shawdowinligth">Suppression d'un artiste</h2>
+
+    <div class="flex justify-center mx-auto text-white gap-10 font-poppins">   
+        <button type="submit"  title="Création" class="bg-Default/me/Gris px-8 py-4 my-7 text-base hover:bg-white hover:text-black">
+            Créer
+        </button>
+        <button class="bg-Default/me/Gris px-8 py-4 my-7 text-base hover:bg-white hover:text-black" >
+          <router-link to="/Liste" >Cancel</router-link>
+        </button>
+    </div>
 </template>
